44d53bce54fccce8b8d69cd7ceb1093af9f31749
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-06-09 18:49:13 +0400

    Implemented Get  for indivindual item by Moniker.

--- Controllers/CampsController.cs
+++ Controllers/CampsController.cs
@@ -30,7 +30,6 @@ namespace CoreCodeCamp.Controllers
             {
                 var results = await _repository.GetAllCampsAsync();
 
-                
                 return _mapper.Map<CampModel[]>(results);
             }
             catch (Exception)
@@ -39,6 +38,24 @@ namespace CoreCodeCamp.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure" );
             } 
 
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
         }
     }
 }
