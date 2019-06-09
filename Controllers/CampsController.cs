67061e7e4ecbd44a5ccab188e23ffd4d8b7f3f1c
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-06-09 23:37:12 +0400

    Implementing Delete

--- Controllers/CampsController.cs
+++ Controllers/CampsController.cs
@@ -130,5 +130,29 @@ namespace CoreCodeCamp.Controllers
             return BadRequest();
 
         }
+
+        [HttpDelete("{moniker}")]
+        public async Task<IActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(moniker);
+                if (oldCamp == null) return NotFound();
+
+                _repository.Delete(oldCamp);
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+
+            }
+
+            return BadRequest("Failed to delet the camp");
+        }
     }
 }
